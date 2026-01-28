fe688c7c697b5def9b8af32e75a8d17d920e4361
UnopposedQuill <pagutiesa2309@gmail.com>
2026-01-27 23:38:39 -0600

    Add Django App and initial model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django'
+    'todos'
 ]
+
+GRAPHENE = {
+    'SCHEMA': 'myapp.schema.schema', # Path to your GraphQL schema
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
